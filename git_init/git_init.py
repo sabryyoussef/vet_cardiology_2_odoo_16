55e1171129ead3ededb103bb91068cbefb11bf61
sabry youssef <vendorah2@gmail.com>
2024-11-10 15:52:52 +0200

    Updated module description with image gallery

---
+++ git_init/git_init.py
@@ -0,0 +1,19 @@
+# #!/bin/bash
+
+# # Initialize the Git repository
+# git init
+
+# # Add all files to the repository
+# git add .
+
+# # Commit the changes with a meaningful message
+# git commit -m "Initial commit"
+
+# # Rename the current branch from master to main
+# git branch -m master main
+
+# # Link the repository to the remote repository
+# git remote add origin https://github.com/your-username/your-repo-name.git
+
+# # Push the changes to the remote repository
+# git push -u origin main
